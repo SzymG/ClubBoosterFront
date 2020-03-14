d3554dc47e771599986dc709184791aaabd6ca4f
Szymon <szymon.gagorek@gmail.com>
2020-03-14 17:10:55 +0100

    Finished app setup, added some graphic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NavigationBar } from '@ionic-native/navigation-bar/ngx';
 
 @Component({
     selector: 'app-root',
@@ -12,14 +13,19 @@ export class AppComponent {
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar
+        private statusBar: StatusBar,
+        private navigationBar: NavigationBar
     ) {
         this.initializeApp();
     }
 
     initializeApp() {
         this.platform.ready().then(() => {
+            const autoHide = true;
+            this.navigationBar.setUp(autoHide);
+            this.navigationBar.hideNavigationBar();
             this.statusBar.styleDefault();
+            this.statusBar.hide();
             this.splashScreen.hide();
         });
     }
