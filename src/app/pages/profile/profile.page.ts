d23389abb6372837e19ae62f54aed73f357306c7
Szymon <szymon.gagorek@gmail.com>
2020-05-11 21:24:03 +0200

    Profile some backend connection

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -7,47 +7,49 @@ import {FullCalendarComponent} from '@fullcalendar/angular';
 import {EventAlertComponent} from '../../components/event-alert/event-alert.component';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ToastService} from '../../services/toast/toast.service';
+import {RequestService} from '../../services/request/request.service';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.page.html',
     styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
     @ViewChild('calendar') calendarComponent: FullCalendarComponent;
     @ViewChild('imageInput') imageInput;
 
     calendarPlugins = [dayGridPlugin];
     locales = [plLocale];
-    // TODO pobieranie eventów do profilu z backendu
-    profileEvents = [
-        {
-            description : 'Przykładowy wyjazd 1',
-            title       : 'W',
-            start       : '2020-04-08T14:00:00',
-            end         : '2020-04-08T18:00:00',
-            color       : '#57ce64',
-        },
-        {
-            description : 'Przykładowy trening',
-            title       : 'T',
-            start       : '2020-04-08T18:30:00',
-            color       : '#fabb3e',
-        },
-        {
-            description: 'Przykładowy wyjazd 2',
-            title : 'W',
-            start : '2020-04-10',
-            color       : '#57ce64',
-        },
-        {
-            description: 'Sparing z ziemniakami',
-            title : 'S',
-            start : '2020-04-25T19:00:00',
-            color       : '#a8319b',
-        }
-    ];
+    profileEvents = [];
+    // // TODO pobieranie eventów do profilu z backendu
+    // profileEvents = [
+    //     {
+    //         description : 'Przykładowy wyjazd 1',
+    //         title       : 'W',
+    //         start       : '2020-04-08T14:00:00',
+    //         end         : '2020-04-08T18:00:00',
+    //         color       : '#57ce64',
+    //     },
+    //     {
+    //         description : 'Przykładowy trening',
+    //         title       : 'T',
+    //         start       : '2020-04-08T18:30:00',
+    //         color       : '#fabb3e',
+    //     },
+    //     {
+    //         description: 'Przykładowy wyjazd 2',
+    //         title : 'W',
+    //         start : '2020-04-10',
+    //         color       : '#57ce64',
+    //     },
+    //     {
+    //         description: 'Sparing z ziemniakami',
+    //         title : 'S',
+    //         start : '2020-04-25T19:00:00',
+    //         color       : '#a8319b',
+    //     }
+    // ];
 
     profile: any;
     image: any;
@@ -60,35 +62,28 @@ export class ProfilePage implements OnInit {
         private readonly eventAlert: EventAlertComponent,
         private readonly formBuilder: FormBuilder,
         private readonly toastService: ToastService,
+        private readonly request: RequestService,
     ) {
         this.profileForm = this.formBuilder.group({
-            id: ['', null],
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            career: ['', null],
+            name: ['', Validators.required],
+            surname: ['', Validators.required],
+            career_description: ['', null],
             email: [{value: null, disabled: true}, null],
-            dateCreation: [{value: null, disabled: true}, null],
-            birthYear: ['', null],
-            description: ['', null],
-            photoUrl: [''],
+            birth_date: ['', null],
+            personal_description: ['', null],
+            avatar_url: [''],
         });
     }
 
-    ngOnInit() {
-        // TODO pobieranie informacji o profilu z backendu
-        this.profile = {
-            id: 7,
-            firstName: 'Janusz',
-            lastName: 'Piłkorz',
-            birthYear: '1997-11-19',
-            dateCreation: '2020-02-24',
-            email: 'janusz.pilkorz@gmail.com',
-            description: 'Jestem sobie Janusz piłkorz',
-            career: 'Byłem w Barcelonie na 50%. Ja chciałem, oni nie.',
-            photoUrl: null
-        };
-
-        this.profileForm.setValue(this.profile);
+    ionViewWillEnter() {
+        this.request.get('events').subscribe((response) => {
+            console.log(response);
+            this.profileEvents = response;
+        });
+        this.request.get('users/current').subscribe((response) => {
+            this.profile = response;
+            this.profileForm.setValue(this.profile);
+        });
     }
 
     logout() {
@@ -100,9 +95,10 @@ export class ProfilePage implements OnInit {
     }
 
     async sendForm() {
-        this.profileForm.controls.photoUrl.setValue(this.imageUrl);
+        // this.profileForm.controls.avatar_url.setValue(this.imageUrl);
         console.log(this.profileForm.value);
         this.toastService.presentToast('Pomyślnie zaktualizowano profil');
+        // TODO update na backendzie
         // if(this.profileForm.valid) {
         //     const profile = this.profileForm;
         //     this.profileService.update(profile, this.image, this.selectedKeywords).then(
--- src/app/services/request/request.service.ts
+++ src/app/services/request/request.service.ts
@@ -1,23 +1,37 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {TranslateService} from '@ngx-translate/core';
 import 'rxjs-compat/add/operator/catch';
+import {UserService} from '../user-service/user.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class RequestService {
     private baseUrl = 'https://murmuring-sea-67113.herokuapp.com/api/';
+    private headers: HttpHeaders;
 
     constructor(
         private readonly http: HttpClient,
         private readonly translateService: TranslateService,
-    ) {}
+        private readonly userService: UserService
+    ) {
+        this.userService.getToken().then((token) => {
+            if (token) {
+                this.headers = new HttpHeaders().append('Authorization', token);
+            }
+        });
+    }
 
     post(url, body, params = {}): Observable<any> {
         const locale = this.translateService.currentLang;
-        return this.http.post<any>(`${this.baseUrl}${url}`, {...body, locale}, params)
+        const options = {
+            params,
+            headers: this.headers
+        };
+
+        return this.http.post<any>(`${this.baseUrl}${url}`, {...body, locale}, options)
             .catch(e => {
                 if (e.status === 401) {
                     return ['unauthorized'];
@@ -26,7 +40,12 @@ export class RequestService {
     }
 
     get(url, params = {}): Observable<any> {
-        return this.http.get<any>(`${this.baseUrl}${url}`, params);
+        const options = {
+            params,
+            headers: this.headers
+        };
+
+        return this.http.get<any>(`${this.baseUrl}${url}`, options);
     }
 
     put(url, body, params = {}): Observable<any> {
